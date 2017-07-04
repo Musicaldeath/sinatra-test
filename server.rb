1dc3897e4859cf754948c602d06808a0b6f10bcb
Alexandre Bouchard <Alexandre Bouchard>
2017-07-04 13:08:15 -0500

    base files

--- server.rb
+++ server.rb
@@ -1,4 +1,13 @@
-require "sinatra"
+require "rubygems"
+require "bundler"
+
+Bundler.require(:default)
+
+require "./app/models/User"
+
+configure do
+  Mongoid.load!("./app/config/dbconfig.yml")
+end
 
 get '/' do
   'ASDASD'
